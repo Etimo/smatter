325bced437be3b37dcd8623df4270900334627c1
Malin Wadholm <70107752+wadholm@users.noreply.github.com>
2025-04-25 11:42:11 +0200

    add feed router

---
+++ backend/src/controllers/users/constants.ts
@@ -0,0 +1,17 @@
+export const users: {
+    username: string;
+    displayName: string;
+}[] = [
+        {
+            username: "philipForsberg",
+            displayName: "Prebenlover123",
+        },
+        {
+            username: "chunkyCat42",
+            displayName: "Round and proud",
+        },
+        {
+            username: "ittybittykittycommittee",
+            displayName: "Mr Meow",
+        },
+    ];
