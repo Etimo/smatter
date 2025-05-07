535f921adcd884fda1ff6e88c84da8f4a3311690
henrikwestoo <henrik.westoo@etimo.se>
2025-05-07 19:37:08 +0200

    running projects

--- packages/react-frontend/src/main.tsx
+++ packages/react-frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
-import ReactDOM from "react-dom/client.js";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.js";
 import "./index.css";
